f1fd28461a1deab825e64d2baa6fbdf0da35a1b9
Rafael Della Giustina <rafael.della64@gmail.com>
2024-02-16 15:13:44 -0300

    Project Final

---
+++ js/config.js
@@ -0,0 +1,11 @@
+function setConfig(){
+    var texts = {
+        "title": "Shopping Controller"
+    };
+
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+
+}
+
+setConfig();
